5a0e9535bf0f2370b70b61b5614463b0125ea6ce
Shanen Cross <shanencross@yahoo.com>
2021-04-19 21:16:48 -0700

    Add Arrange, Act, Assert comments

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -12,47 +12,66 @@ namespace ToDoList.Tests {
     
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item() {
+      // Arrange and Act
       Item newItem = new Item("test");
+
+      // Assert
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
 
     [TestMethod]
     public void GetDescription_ReturnsDescription_String() {
+      // Arrange
       string description = "Walk the dog.";
+      
+      // Act
       Item newItem = new Item(description);
       string result = newItem.Description;
+      
+      // Asset
       Assert.AreEqual(description, result);
     }
 
     [TestMethod]
     public void SetDescription_SetsDescription_String() {
+      // Arrange
       string description = "Walk the dog.";
       Item newItem = new Item(description);
 
+      // Act
       string updatedDescription = "Do the dishes";
       newItem.Description = updatedDescription;
       string result = newItem.Description;
       
+      // Assert
       Assert.AreEqual(updatedDescription, result);
     }
     
     [TestMethod]
     public void GetAll_ReturnsEmptyList_ItemList() {
+      // Arrange
       List<Item> newList = new List<Item> {};
+      
+      // Act
       List<Item> result = Item.GetAll();
+      
+      // Assert
       CollectionAssert.AreEqual(newList, result);
     }
 
     [TestMethod]
     public void GetAll_ReturnsItems_ItemList() {
+      // Arrange
       string description01 = "Walk the dog";
       string description02 = "Wash the dishes";
       Item newItem1 = new Item(description01);
       Item newItem2 = new Item(description02);
       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
+      // Act
       List<Item> result = Item.GetAll();
 
+      // Assert
       CollectionAssert.AreEqual(newList, result);
     }
   }
