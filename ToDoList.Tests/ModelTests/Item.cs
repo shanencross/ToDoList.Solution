7b47e564a67c7de58473e3e5be13659dcfe92257
Shanen Cross <shanencross@yahoo.com>
2021-04-19 20:25:51 -0700

    Add and pass Item constructor test

--- ToDoList.Tests/ModelTests/Item.cs
+++ ToDoList.Tests/ModelTests/Item.cs
@@ -5,8 +5,9 @@ namespace ToDoList.Tests {
   [TestClass]
   public class ItemTests {
     [TestMethod]
-    public void Item_SomeCondition_ExpectedValue() {
-      Assert.AreEqual(true, false);
+    public void ItemConstructor_CreatesInstanceOfItem_Item() {
+      Item newItem = new Item();
+      Assert.AreEqual(typeof(Item), newItem.GetType());
     }
   }
 }
