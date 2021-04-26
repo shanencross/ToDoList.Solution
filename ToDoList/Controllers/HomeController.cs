2528816437f289124126926ba8846e19bf80ee7d
Shanen Cross <shanencross@yahoo.com>
2021-04-26 10:39:09 -0700

    Add for route, for home page

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -5,6 +5,11 @@ namespace ToDoList.Controllers
 {
   public class HomeController : Controller
   {
-    
+    [Route("/")]
+    public ActionResult Index()
+    {
+      Item starterItem = new Item("Add first item to To Do List");
+      return View(starterItem);
+    }
   }
 }
