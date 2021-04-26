ac8235f4db56ea9f04268db2d848966a1d15aba7
Shanen Cross <shanencross@yahoo.com>
2021-04-26 10:36:39 -0700

    Remove console app coe and add web code for Program file

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,47 +1,20 @@
-using System;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList 
 {
   public class Program 
   {
-    public static void Main() 
+    public static void Main(string[] args) 
     {
-      Console.WriteLine("Welcome to the To Do List!");
-
-      bool runProgram = true;
-      while (runProgram) 
-      {
-        Console.WriteLine("Would you like to add an item to your list or view your list? (Add/View)");
-        string userInput = Console.ReadLine().Trim().ToLower();
-
-        if (userInput == "add") 
-        {
-          Console.WriteLine("Please enter the description for the new item.");
-          string description = Console.ReadLine();
-          Item newItem = new Item(description);
-          Console.Write("'" + newItem.Description + "' has been added to your list. ");
-        } 
-        else if (userInput == "view") 
-        {
-          if (Item.Instances.Count == 0) 
-          {
-            Console.WriteLine("No items added yet.");
-          }
-          
-          for (int i = 0; i < Item.Instances.Count; i++) 
-          {
-            int num = i + 1;
-            Item currentItem = Item.Instances[i];
-           
-            Console.WriteLine(num + ". " + currentItem.Description);
-          }
-        }
-        else 
-        {
-          runProgram = false;
-        }
-      }
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+      
+      host.Run();
     }
-  } 
+
 }
