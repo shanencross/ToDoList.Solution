f6b492ff85dcb897921460d5135290fb1e5288e1
Shanen Cross <shanencross@yahoo.com>
2021-05-04 20:03:43 -0700

    Add Category model
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

---
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Category
+  {
+    private static List<Category> _instances = new List<Category> {};
+    public int Id { get; }
+    public List<Item> Items { get; set; }
+  }
+}
