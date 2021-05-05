c4dae3aecd2b8f0907f550b7aebafc339d19f3ff
Shanen Cross <shanencross@yahoo.com>
2021-05-04 20:04:30 -0700

    Add Name property to Category
    Co-authored-by: Cat Denton <cat.denton.dev@gmail.com>

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -5,6 +5,7 @@ namespace ToDoList.Models
   public class Category
   {
     private static List<Category> _instances = new List<Category> {};
+    public string Name { get; set; }
     public int Id { get; }
     public List<Item> Items { get; set; }
   }
