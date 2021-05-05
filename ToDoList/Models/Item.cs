eab247592b4e77c179671eb895424e00771c96b7
Shanen Cross <shanencross@yahoo.com>
2021-05-04 19:08:49 -0700

    Add Id property and Find method to Item
    Co-authored-by: Cat Denton <Cat-Denton@users.noreply.github.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,16 +5,23 @@ namespace ToDoList.Models
   public class Item 
   {
     public string Description { get; set; }
+    public int Id { get; }
     public static List<Item> Instances { get; } = new List<Item> {};
     public Item(string description) 
     {
       Description = description;
       Instances.Add(this);
+      Id = Instances.Count;
     }
 
     public static void ClearAll() 
     {
       Instances.Clear();
+    }
+
+    public static Item Find(int searchId)
+    {
+      return Instances[searchId-1];
     }
   }
 }
