e23c94bc951d55553e1d0acd356ec1c091b8cdea
Cat-Denton <willwdenton@gmail.com>
2021-05-05 19:33:20 -0700

    add tests to verify that Category class can return properties, added ClearAll method and disposer
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- ToDoList.Tests/ModelTests/CategoryTests.cs
+++ ToDoList.Tests/ModelTests/CategoryTests.cs
@@ -7,13 +7,42 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class CategoryTests
+  public class CategoryTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Category.ClearAll();
+    }
+    
     [TestMethod]
     public void CategoryConstructor_CreatesInstanceOfCategory_Category()
     {
       Category newCategory = new Category("test category");
       Assert.AreEqual(typeof(Category), newCategory.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+      string result = newCategory.Name;
+      Assert.AreEqual(name, result);
+    }
+
+      [TestMethod]
+    public void GetId_ReturnsCategoryId_Int()
+    {
+      //Arrange
+      string name = "Test Category";
+      Category newCategory = new Category(name);
+
+      //Act
+      int result = newCategory.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
     }
   }
 }
