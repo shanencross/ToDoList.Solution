1b39d68d28bc097cb9d23e50fa2d15943ce717a2
Shanen Cross <shanencross@yahoo.com>
2021-05-09 11:29:18 -0700

    Add route and view for displaying items in category

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -20,13 +20,22 @@ namespace ToDoList.Controllers
       return View();
     }
 
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.Items;
+      model.Add("category", selectedCategory);
+      model.Add("items", categoryItems);
+      return View(model);
+    }
+
     [HttpPost("/categories")]
     public ActionResult Create(string categoryName)
     {
       Category newCategory = new Category(categoryName);
       return RedirectToAction("Index");
     }
-
-
   }
 }
