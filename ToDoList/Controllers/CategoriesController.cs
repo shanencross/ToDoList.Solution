2e0c23ebf44e72e7478e15f196f73da58f652ffc
Shanen Cross <shanencross@yahoo.com>
2021-05-09 11:18:05 -0700

    Add view and route for new category form page

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -13,5 +13,13 @@ namespace ToDoList.Controllers
       List<Category> allCategories = Category.GetAll();
       return View(allCategories);
     }
+
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+
   }
 }
