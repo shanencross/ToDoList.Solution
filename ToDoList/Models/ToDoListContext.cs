481a1424f7f5068eb1f3d83d20f0a49db8510de8
Shanen Cross <shanencross@yahoo.com>
2021-05-16 10:37:26 -0700

    Add categories property and lazy loading
    Co-authored-by: Cat Denton <Cat-Denton@users.noreply.github.com>

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,7 +4,13 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
 
     public ToDoListContext(DbContextOptions options): base(options) { }
   }
