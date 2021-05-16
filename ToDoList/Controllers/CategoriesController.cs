313c7804ded9b820c3faa1fc9c5af214299c9b25
Shanen Cross <shanencross@yahoo.com>
2021-05-16 10:59:29 -0700

    Add Categories Controller
    Co-authored-by: Cat Denton <Cat-Denton@users.noreply.github.com>

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,12 +1,25 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db; 
+    }
+
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
+
 //     [HttpGet("/categories")]
 //     public ActionResult Index()
 //     {
@@ -51,5 +64,5 @@
 //       model.Add("category", foundCategory);
 //       return View("Show", model);
 //     }
-//   }
-// }
+  }
+}
