809113171224141b23a291cbc5fff28b6f865595
Shanen Cross <shanencross@yahoo.com>
2021-06-06 14:03:51 -0700

    Set up authorization for items
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -4,21 +4,32 @@ using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
 
 namespace ToDoList.Controllers
 {
+  [Authorize]
   public class ItemsController : Controller
   {
     private readonly ToDoListContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public ItemsController(ToDoListContext db)
+    public ItemsController(UserManager<ApplicationUser> userManager, ToDoListContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View(_db.Items.ToList());
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userItems);
     }
 
     public ActionResult Create()
@@ -28,8 +39,11 @@ namespace ToDoList.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Item item, int categoryId)
+    public async Task<ActionResult> Create(Item item, int categoryId)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      item.User = currentUser;
       _db.Items.Add(item);
       _db.SaveChanges();
       if (categoryId != 0)
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Models
     public int ItemId { get; set; }
     public string Description { get; set; }
     public int CategoryId { get; set; }
+    public virtual ApplicationUser User { get; set; }
     public virtual ICollection<CategoryItem> JoinEntities { get; }
 
     public Item()
